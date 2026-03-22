3d8c106141042bc720461b759835aaf8b0d8bccd
krishpranav <krisna.pranav@gmail.com>
2026-03-22 15:13:47 +0530

    hooks: useHaptics

--- src/hooks/useHaptics.ts
+++ src/hooks/useHaptics.ts
@@ -5,4 +5,26 @@ export const useHaptics = () => {
     const light = useCallback(() => {
         Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
     }, []);
-}
+
+    const medium = useCallback(() => {
+        Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
+    }, []);
+
+    const heavy = useCallback(() => {
+        Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy);
+    }, []);
+
+    const success = useCallback(() => {
+        Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
+    }, []);
+
+    const error = useCallback(() => {
+        Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error);
+    }, []);
+
+    const warning = useCallback(() => {
+        Haptics.notificationAsync(Haptics.NotificationFeedbackType.Warning);
+    }, []);
+
+    return { light, medium, heavy, success, error, warning };
+};
